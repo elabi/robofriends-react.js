a5f002e4a19994bf9497fd4291f3e1129356c89a
elabi <ELABI.STENG@OUTLOOK.COM>
2018-04-08 23:48:26 +0300

    Added SearchBar and 2Way Binding between data and Searchbox

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Hello from './Hello';
+import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import 'tachyons';
+import {robots} from './robots';
 
-ReactDOM.render(<Hello greeting = {'Hello, ' + 'React Ninja'} />, document.getElementById('root'));
+ReactDOM.render( <App />, document.getElementById('root'));
 registerServiceWorker();
